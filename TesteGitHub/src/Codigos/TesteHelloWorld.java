ba27726e21795a1c4f95532631ef5b5210eead22
candinhojr <candinholuiz@gmail.com>
2017-08-03 19:19:25 -0300

    Criado projeto e classe java

---
+++ TesteGitHub/src/Codigos/TesteHelloWorld.java
@@ -0,0 +1,23 @@
+package Codigos;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Candinho Jr
+ */
+public class TesteHelloWorld {
+    
+    public static void main(String[] args) {
+      TesteHelloWorld hi = new TesteHelloWorld();
+      hi.imprimeTexto("Alô Professor!");
+  }
+ 
+  public void imprimeTexto(String texto){
+      System.out.println(texto);
+  }
+}
