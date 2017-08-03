fd8c5fa2427325982803bb326f0a569b56a7fef5
candinhojr <candinholuiz@gmail.com>
2017-08-03 19:56:58 -0300

    Teste 02

--- TesteGitHub/src/Pacote/Teste.java
+++ TesteGitHub/src/Pacote/Teste.java
@@ -1,20 +1,18 @@
-package Codigos;
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package Pacote;
 
 /**
  *
  * @author Candinho Jr
  */
-public class TesteHelloWorld {
-    
+public class Teste {
     public static void main(String[] args) {
-      TesteHelloWorld hi = new TesteHelloWorld();
-      hi.imprimeTexto("Alô Professor!!!");
+      Teste hi = new Teste();
+      hi.imprimeTexto("Teste 2!");
   }
  
   public void imprimeTexto(String texto){
